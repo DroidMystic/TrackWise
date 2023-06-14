29c7a775b6569ec32e6ebac61d3dbc63cca66d63
chamvamshi <kasaramvamshi7143@gmail.com>
2023-06-14 14:06:48 +0530

    splash

---
+++ app/src/main/java/com/example/trackwise/splashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.trackwise
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+       supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
